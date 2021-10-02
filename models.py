1c151770be0a003e42ba712a4a150726efd54c4a
breadbread1984 <breadbread1984@163.com>
2021-10-02 22:43:38 +0800

    update

--- models.py
+++ models.py
@@ -70,6 +70,29 @@ def GeneratorTwo():
   results = tf.keras.layers.Reshape((8,8,3))(results);
   return tf.keras.Model(inputs = noise, outputs = results);
 
+def PyrUp(channels):
+  inputs = tf.keras.Input((None, None, channels)); # inputs.shape = (batch, height, width, channel)
+  # 1) dilate with zeros
+  results = tf.keras.layers.Lambda(lambda x: tf.stack([x, tf.zeros_like(x)], axis = -1))(inputs); # results.shape = (batch, height, width, channel, 2)
+  results = tf.keras.layers.Lambda(lambda x: tf.transpose(x, (0, 1, 4, 2, 3)))(results); # results.shape = (batch, height, 2, width, channel)
+  results = tf.keras.layers.Lambda(lambda x: tf.reshape(x, (tf.shape(x)[0], -1, tf.shape(x)[3], tf.shape(x)[4])))(results); # results.shape = (batch, height*2, width, channel)
+  results = tf.keras.layers.Lambda(lambda x: tf.stack([x, tf.zeros_like(x)], axis = -1))(results); # results.shape = (batch, height * 2, width, channels, 2)
+  results = tf.keras.layers.Lambda(lambda x: tf.transpose(x, (0, 1, 2, 4, 3)))(results); # results.shape = (batch, height * 2, width, 2, channels)
+  results = tf.keras.layers.Lambda(lambda x: tf.reshape(x, (tf.shape(x)[0], tf.shape(x)[1], -1, tf.shape(x)[4])))(results); # results.shape = (batch, height * 2, width * 2, channels);
+  results = tf.keras.layers.Lambda(lambda x: x[:,:-1,:-1,:])(results); # results.shape = (batch, height * 2 - 1, width * 2 - 1, channels)
+  # 2) reflect padding
+  results = tf.keras.layers.Lambda(lambda x: tf.pad(x, [[0,0],[1,1],[1,1],[0,0]], "REFLECT"))(results);
+  # 3) gaussian filtering
+  #gaussian = tf.keras.layers.Lambda(lambda x: )(results)
+  return tf.keras.Model(inputs = inputs, outputs = results);
+
+def Trainer():
+  noise2 = tf.keras.Input((100,));
+  noise1 = tf.keras.Input((16,16,1));
+  noise0 = tf.keras.Input((32,32,1));
+  sample0 = GeneratorTwo()(noise2);
+#  sample1 = 
+
 if __name__ == "__main__":
 
   import numpy as np;
@@ -91,3 +114,7 @@ if __name__ == "__main__":
   gen_2 = GeneratorTwo();
   results = gen_2(np.random.normal(size = (4,100)));
   print(results.shape);
+  pyrup = PyrUp(2);
+  results = pyrup(tf.ones((1,2,2,2)));
+  print(results[0,:,:,0]);
+  print(results[0,:,:,1]);
