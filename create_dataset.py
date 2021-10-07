2ba7119dc510edf0aa1585a86606833e6114cbad
yi.xie <yi.xie@enflame-tech.com>
2021-10-07 22:26:12 +0800

    update

--- create_dataset.py
+++ create_dataset.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 
+from absl import app, flags;
 import tensorflow as tf;
 import tensorflow_datasets as tfds;
 from models import PyrUp;
+
+FLAGS = flags.FLAGS;
+
+def add_options():
+  flags.DEFINE_bool('test', False, help = 'test dataset input pipeline');
 
 def download():
 
   cifar10_builder = tfds.builder('cifar10');
   cifar10_builder.download_and_prepare();
 
-def parse_function(features):
+def parse_sample(features):
   _, img, label = features['id'], features['image'].numpy(), features['label'];
   gaussian = list();
   laplacian = list();
@@ -29,8 +35,31 @@ def load_datasets():
 
   trainset = tfds.load(name = 'cifar10', split = 'train', download = False);
   testset = tfds.load(name = 'cifar10', split = 'test', download = False);
+  trainset = trainset.map(parse_sample);
+  testset = testset.map(parse_sample);
   return trainset, testset;
+
+def main(unused_argv):
+  if FLAGS.test:
+    import cv2;
+    trainset, testset = load_datasets();
+    cv2.namedWindow('gaussian0', cv2.WINDOW_NORMAL);
+    cv2.namedWindow('gaussian1', cv2.WINDOW_NORMAL);
+    cv2.namedWindow('gaussian2', cv2.WINDOW_NORMAL);
+    cv2.namedWindow('laplacian0', cv2.WINDOW_NORMAL);
+    cv2.namedWindow('laplacian1', cv2.WINDOW_NORMAL);
+    for sample, label in trainset_iter:
+      real_gaussian0, real_gaussian1, real_gaussian2, real_laplacian0, real_laplacian1 = sample;
+      cv2.imshow('gaussian0', real_gaussian0);
+      cv2.imshow('gaussian1', real_gaussian1);
+      cv2.imshow('gaussian2', real_gaussian2);
+      cv2.imshow('laplacian0', real_laplacian0);
+      cv2.imshow('laplacian1', real_laplacian1);
+      cv2.waitKey();
+  else:
+    download();
 
 if __name__ == "__main__":
 
-  download();
+  add_options();
+  app.run(main);
--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from os.path import join;
 from absl import app, flags;
 import tensorflow as tf;
 from models import Trainer;
-from create_datasets import load_datasets, parse_function;
+from create_datasets import load_datasets;
 
 FLAGS = flags.FLAGS;
 
@@ -17,6 +17,9 @@ def add_options():
 def minimize(_, loss):
   return loss;
 
+class SummaryCallback(tf.keras.callbacks.Callback):
+  def __init__(self, eval_freq = 100):
+      
 def main(unused_argv):
   if exists(FLAGS.checkpoint):
     trainer = tf.keras.models.load_model(FLAGS.checkpoint, custom_objects = {'tf': tf, 'minimize': minimize}, compile = True);
@@ -26,16 +29,13 @@ def main(unused_argv):
     optimizer = tf.keras.optimizers.Adam(FLAGS.lr);
     trainer.compile(optimizer = optimizer,
                     loss = {'loss0': minimize, 'loss1': minimize, 'loss2': minimize});
-  class SummaryCallback(tf.keras.callbacks.Callback):
-    def __init__(self, eval_freq = 100):
-      
   trainset, testset = load_datasets();
-  trainset = trainset.map(parse_function).shuffle(FLAGS.batch_size).batch(FLAGS.batch_size).prefetch(tf.data.experimental.AUTOTUNE);
-  testset = testset.map(parse_function).shuffle(FLAGS.batch_size).batch(FLAGS.batch_size).prefetch(tf.data.experimental.AUTOTUNE);
+  trainset = trainset.shuffle(FLAGS.batch_size).batch(FLAGS.batch_size).prefetch(tf.data.experimental.AUTOTUNE);
+  testset = testset.shuffle(FLAGS.batch_size).batch(FLAGS.batch_size).prefetch(tf.data.experimental.AUTOTUNE);
   callbacks = [
     tf.keras.callbacks.TensorBoard(log_dir = FLAGS.checkpoint),
     tf.keras.callbacks.ModelCheckpoint(filepath = join(FLAGS.checkpoint, 'ckpt'), save_freq = 1000),
-    SummaryCallback()
+    SummaryCallback(trainer);
   ];
   trainer.fit(trainset, epochs = FLAGS.epochs, validation_data = testset, callbacks = callbacks);
 
